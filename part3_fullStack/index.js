87d80998f3736bf0782847928e9e789315a1c7a1
franco22bellido <franco.bellido22@gmail.com>
2024-04-01 03:44:13 -0300

    introducing express.js methods

--- part3_fullStack/index.js
+++ part3_fullStack/index.js
@@ -1,10 +1,51 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response)=> {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('Hello World')
+const notes = [
+    {
+        "id": 1,
+        "content": "estudiar pruebas e2e",
+        "date": "1/04/2024",
+        "important": true
+    },
+    {
+        "id": 2,
+        "content": "estudiar cypress",
+        "date": "2/04/2024",
+        "important": true
+    },
+    {
+        "id": 3,
+        "content": "practicar sockets con nestJs",
+        "date": "07/04/2024",
+        "important": true
+    }
+]
+
+app.get('/', (req, res)=> {
+    res.send('<h1>hello world</h1>')
+})
+
+app.get('/api/notes', (req, res)=> {
+    res.json(notes)
+})
+app.get('/api/notes/:id', (req, res)=> {
+    const id = parseInt(req.params.id)
+    const note = notes.find((note)=> note.id === id)
+    if(!note){
+        res.status(404).end()
+    }
+    res.json({
+        note
+    })
+})
+app.delete('/api/notes/:id', (req, res)=> {
+    const id = parseInt(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+    res.status(204).end()
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, ()=> {
+    console.log(`Server on port ${PORT}`)
+})
