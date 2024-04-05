9d42be402d5aecdff5a65ea83add90915f04df91
franco22bellido <franco.bellido22@gmail.com>
2024-04-04 21:20:32 -0300

    testing the endpoint /api/notes in diferent cases.

--- part3_fullStack/tests/notes.test.js
+++ part3_fullStack/tests/notes.test.js
@@ -1,14 +1,50 @@
 const supertest = require('supertest')
 const { app, server } = require('../index.js')
 const mongoose = require('../mongo.js')
-
+const Note = require('../models/NoteSchema.js')
 const api = supertest(app)
+
+const initialNotes = [
+    {
+        content: 'learning FullStack JS with midudev',
+        important: true,
+        date: new Date()
+    },
+    {
+        content: 'siguelo en https://midu.tube',
+        important: true,
+        date: new Date()
+    }
+]
+beforeEach(async () => {
+    await Note.deleteMany({})
+    const note1 = new Note(initialNotes[0])
+    await note1.save()
+    const note2 = new Note(initialNotes[1])
+    await note2.save()
+})
 
 test('notes are returned as json', async () => {
     await api
         .get('/api/notes')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+test('there are two notes', async () => {
+    const response = await api.get('/api/notes')
+    expect(response.body).toHaveLength(initialNotes.length)
+})
+test('the first note is about midudev', async () => {
+    const response = await api.get('/api/notes')
+    expect(response.body[0].content).toBe('learning FullStack JS with midudev')
+})
+
+test('contain the response.body array a specific words?', async () => {
+    const response = await api.get('/api/notes')
+
+    const contents = response.body.map(note => note.content)
+
+    expect(contents).toContain('learning FullStack JS with midudev')
 })
 
 afterAll(() => {
--- part3_fullStack/tests/palindrome.test.js
+++ part3_fullStack/tests/palindrome.test.js
@@ -1,16 +1,16 @@
 const { palindrome } = require('../utils/for_testing.js')
 
-test('palindrome, normal use', () => {
+test.skip('palindrome, normal use', () => {
     const result = palindrome('midudev')
 
     expect(result).toBe('vedudim')
 })
-test('palindrome of empty string', () => {
+test.skip('palindrome of empty string', () => {
     const result = palindrome('')
 
     expect(result).toBe('')
 })
-test('palindrome when type is not a "string"', () => {
+test.skip('palindrome when type is not a "string"', () => {
     const result = palindrome()
 
     expect(result).toBeUndefined()
