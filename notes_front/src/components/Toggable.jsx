bdf9e0b9cd36e72b678b0dbc0417c66932baccff
franco22bellido <franco.bellido22@gmail.com>
2024-04-15 22:17:20 -0300

    mi propia versión de un toggable para mostrar/ocultar un componente

--- notes_front/src/components/Toggable.jsx
+++ notes_front/src/components/Toggable.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react"
 const Toggable = ({children}) => {
 
+  const [showChildren, setShowChildren, ] = useState(false)
+  const [display, setDisplay] = useState({})
 
+  const handleShowChildren = ()=> {
+    setShowChildren((show)=> !show)
+  }
 
+  const showDisplay = ()=>{
+    if(showChildren){
+        setDisplay({display: 'block'})
+    }else{
+        setDisplay({display: 'none'})
+    }
+  }
+  useEffect(()=> {
+    showDisplay()
+  }, [showChildren])
   return (
     <div>
-      {children}
+        <div style={display}>
+            {children}
+        </div>
+      <button onClick={()=> handleShowChildren()}>{showChildren ? 'ocultar' : 'mostrar'}</button>
     </div>
   )
 }
